9e647f5e7a9ec04636c178493a9293de36998e0e
aalbaan <94935345+aalbaan@users.noreply.github.com>
2024-12-26 15:03:32 +0100

    courseRVAdapter

--- app/src/main/java/com/example/firebasecrudapplication/MainActivity.java
+++ app/src/main/java/com/example/firebasecrudapplication/MainActivity.java
@@ -1,10 +1,30 @@
 package com.example.firebasecrudapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView courseRV;
+    private ProgressBar loadingPB;
+    private FloatingActionButton addPAB;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private ArrayList<CourseRVModel> courseRVModelArrayList;
+    private RelativeLayout homeRL;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
